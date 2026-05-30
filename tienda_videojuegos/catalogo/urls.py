5c7ed55f5f5cd811d3ee12fd5011b4996f378d9c
Miguel Mesa <miguelangelmesagarzon@gmail.com>
2026-05-30 18:58:19 -0500

    feat: añadir imagenes al modelo y paginación

--- tienda_videojuegos/catalogo/urls.py
+++ tienda_videojuegos/catalogo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
  #ruta contacto: Llama a la vista de contacto
+    path('', views.lista_juegos, name='lista_juegos')
 ]
--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from catalogo.models import Juego
+
 
 # Create your views here.
+def lista_juegos(request):
+    juegos = Juego.objects.all()
+
+    paginator = Paginator(juegos,6)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    contexto_catalogo_juegos = {'lista_juegos': page_obj}
+
+    return render(request, 'catalogo/lista_juegos.html', contexto_catalogo_juegos)
